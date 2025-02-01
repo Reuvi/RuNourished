9f957dc622354a0a18420128e3781b93687939f2
IsaacS <ikeysasson@gmail.com>
2025-02-01 14:10:08 -0500

    Test

--- Project/frontend/src/components/Home.js
+++ Project/frontend/src/components/Home.js
@@ -2,7 +2,7 @@ function Home() {
 
     return (
         <div>
-            <h1 class="text-paleYellow"> Hello World</h1>
+            <h1 class="text-paleYellow"> Ikey Test World</h1>
         </div>
     )
 };
