31974a48df1737b0ceb3907ef7dc230e12e33313
Reuvi <reuvi_israeli@icloud.com>
2025-02-01 16:10:55 -0500

    updated enpoint no age

--- Project/backend/models/User.js
+++ Project/backend/models/User.js
@@ -14,7 +14,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  age: { type: Number, min: 18 },
+  age: { type: Number, default: 0},
   isAdmin: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now }
 });
