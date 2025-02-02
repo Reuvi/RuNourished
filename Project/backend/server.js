b4ccb93a142e1692986c6463310f09d3124413bc
Reuvi <reuvi_israeli@icloud.com>
2025-02-02 06:05:31 -0500

    http access

--- Project/backend/server.js
+++ Project/backend/server.js
@@ -40,7 +40,7 @@ app.get("*", (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
