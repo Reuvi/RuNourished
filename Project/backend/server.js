3e51fc8c5590bc80e75588df691f7417348a198c
Reuvi <reuvi_israeli@icloud.com>
2025-02-02 06:55:14 -0500

    Added local hosting

--- Project/backend/server.js
+++ Project/backend/server.js
@@ -41,6 +41,6 @@ app.get("*", (req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 80;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
